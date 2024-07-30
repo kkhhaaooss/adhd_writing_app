478783624eaaee2a6097f9e75c78730df5a042ce
Russell Tripp <russell.tripp.infusion@gmail.com>
2024-07-29 21:06:27 -0400

    Changes timer package to circular_countdown_timer

--- lib/turn_timer.dart
+++ lib/turn_timer.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:circular_timer/circular_timer.dart';
+import 'package:circular_countdown_timer/circular_countdown_timer.dart';
 
 class TurnTimer extends StatefulWidget {
   const TurnTimer({super.key});
@@ -11,11 +11,47 @@ class TurnTimer extends StatefulWidget {
 class _TurnTimerState extends State<TurnTimer> {
   @override
   Widget build(BuildContext context) {
-    final timerRadius = MediaQuery.sizeOf(context).height * .1;
-    return CircularTimer(
-      repeat: true,
-      duration: const Duration(minutes: 15),
-      radius: timerRadius,
+    return CircularCountDownTimer(
+      duration: 10,
+      initialDuration: 0,
+      controller: CountDownController(),
+      width: MediaQuery.of(context).size.width / 2,
+      height: MediaQuery.of(context).size.height / 2,
+      ringColor: Colors.grey[300]!,
+      ringGradient: null,
+      fillColor: Colors.purpleAccent[100]!,
+      fillGradient: null,
+      backgroundColor: Colors.purple[500],
+      backgroundGradient: null,
+      strokeWidth: 20.0,
+      strokeCap: StrokeCap.round,
+      textStyle: const TextStyle(
+        fontSize: 33.0,
+        color: Colors.white,
+        fontWeight: FontWeight.bold,
+      ),
+      textAlign: TextAlign.center,
+      textFormat: CountdownTextFormat.S,
+      isReverse: false,
+      isReverseAnimation: false,
+      isTimerTextShown: true,
+      autoStart: false,
+      onStart: () {
+        debugPrint('Countdown Started');
+      },
+      onComplete: () {
+        debugPrint('Countdown Ended');
+      },
+      onChange: (String timeStamp) {
+        debugPrint('Countdown Changed $timeStamp');
+      },
+      timeFormatterFunction: (defaultFormatterFunction, duration) {
+        if (duration.inSeconds == 0) {
+          return "Start";
+        } else {
+          return Function.apply(defaultFormatterFunction, [duration]);
+        }
+      },
     );
   }
 }
